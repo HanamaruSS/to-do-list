118b8a6372eead1014368ec2ece85fe60fe388a7
HanamaruSS <indra.rg@gmail.com>
2016-10-06 00:18:20 +0800

    add some tags

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-    	<link type='text/css' rel='stylesheet' href='style.css'/>
-	<title>To-do List</title>
+        <meta charset='utf-8'>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+        <title>To-do List</title>
     </head>
 
     <body>
@@ -10,41 +11,61 @@
         include 'header.php';
         ?>
 
-	<h1>Things that need to be done</h1>
+        <h1>Things that need to be done</h1>
 
-	<?php
-	include 'db.php';
+        <?php
+        include 'db.php';
 
         $order = htmlspecialchars($_GET["o"]);
         if ($order === "") {
                 $order = "deadline";
         }
-	$sql = "SELECT task, deadline, done FROM todo_list ORDER BY $order";
-	$result = $conn->query($sql);
+        $sql = "SELECT task, deadline, done, tag
+                FROM todo_list LEFT JOIN junction_table
+                ON junction_table.todo_id = todo_list.id
+                LEFT JOIN tag
+                ON junction_table.tag_id = tag.id
+                ORDER BY todo_list.$order";
+        // do we need OUTER for LEFT OUTER JOIN?
 
-	if ($result->num_rows > 0) {
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
                 echo "<table> <tr>";
                 echo "<th><a href='/?o=task'>Task</a></th>";
                 echo "<th><a href='/?o=deadline'>Deadline</a></th>";
-                echo "<th><a href='/?o=done'>Is it done?</a></th> </tr>";
+                echo "<th><a href='/?o=done'>Is it done?</a></th>";
+                echo "<th>Tag(s)</th></tr>";
 
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
-		if($row["done"]){
-			echo "<tr class='done'>";
-		} else {
-			echo "<tr class='not-done'>";
-		}
-		echo "<td>".$row["task"]. "</td><td>" . $row["deadline"] . "</td><td>" . $row["done"]. "</td></tr>";
-	    }
-	    echo "</table>";
+                        if($row["done"]){
+                                echo "<tr class='done'>";
+                        } else {
+                                echo "<tr class='not-done'>";
+                        }
 
-	} else {
-	    echo "No Task to be done. Hurray! \o/";
-	}
+                        echo "<td>".$row["task"]. "</td><td>";
+                        echo $row["deadline"] . "</td><td>";
 
-	$conn->close();
-	?>
+                        if ($row["done"] == true) {
+                                echo "Done!! :)";
+                        } else {
+                                echo "Not yet... :(";
+                        }
+
+                        echo "</td><td>" . $row["tag"];
+
+                        echo "</td></tr>";
+                }
+                echo "</table>";
+
+        } else {
+                echo "No Task to be done. Hurray! \o/";
+        }
+
+        $conn->close();
+        ?>
 
     </body>
 </html>
--- todo.sql
+++ todo.sql
@@ -40,6 +40,52 @@ INSERT INTO `admin` VALUES (1,'indra','raditya'),(2,'herbert','ilhan'),(3,'antho
 UNLOCK TABLES;
 
 --
+-- Table structure for table `junction_table`
+--
+
+DROP TABLE IF EXISTS `junction_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `junction_table` (
+  `todo_id` int(11) DEFAULT NULL,
+  `tag_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `junction_table`
+--
+
+LOCK TABLES `junction_table` WRITE;
+/*!40000 ALTER TABLE `junction_table` DISABLE KEYS */;
+INSERT INTO `junction_table` VALUES (1,1),(1,2),(1,3),(4,1),(5,1),(5,4),(6,4),(2,5);
+/*!40000 ALTER TABLE `junction_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(11) DEFAULT NULL,
+  `tag` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` VALUES (1,'important'),(2,'CommIT'),(3,'programming'),(4,'random'),(5,'chores');
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `todo_list`
 --
 
@@ -61,7 +107,7 @@ CREATE TABLE `todo_list` (
 
 LOCK TABLES `todo_list` WRITE;
 /*!40000 ALTER TABLE `todo_list` DISABLE KEYS */;
-INSERT INTO `todo_list` VALUES (1,'make to-do list','2016-10-04',0,1),(2,'do laundry','2016-09-25',1,1),(3,'return iron to Bona','2016-09-30',0,1),(4,'return transponder to Anthony','2016-09-24',1,1),(5,'stalk Herbert','2100-12-31',0,1);
+INSERT INTO `todo_list` VALUES (1,'make to-do list','2016-10-04',0,1),(2,'do laundry','2016-09-25',1,1),(3,'return iron to Bona','2016-09-30',0,1),(4,'return transponder to Anthony','2016-09-24',1,1),(5,'stalk Herbert','2100-12-31',0,1),(6,'asdfasdf','2100-11-15',1,1);
 /*!40000 ALTER TABLE `todo_list` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -74,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-25 20:40:10
+-- Dump completed on 2016-10-06  0:15:59
