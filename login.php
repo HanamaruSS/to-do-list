06d2f8102b6dc60d55474504ebfe12b56d067f77
HanamaruSS <indra.rg@gmail.com>
2016-10-06 13:26:02 +0800

    can login but...

--- login.php
+++ login.php
@@ -10,8 +10,8 @@
 	<body>
 		<form class="form-signin" action="login_submit.php" method="post">
 			<h2 class="form-signin-heading">Login</h2>
-			<label for="username" class="sr-only">Username or E-mail Address</label>
-			<input id="username" name="username" class="form-control" placeholder="Username or e-mail address" required>
+			<label for="username" class="sr-only">Username</label>
+			<input id="username" name="username" class="form-control" placeholder="Username" required>
 			<label for="password" class="sr-only">Password</label>
                         <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
                         <button id="form-submit" class="btn-submit" type="submit">Sign in</button>
---
+++ login_submit.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang='en'>
+        <head>
+                <meta charset='utf-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+                <title>To-do List</title>
+
+                <link href='style.css' rel='stylesheet'>
+        </head>
+
+        <body>
+                <?php include 'header.php' ?>
+                <?php
+                include 'db.php';
+
+                if ($_SERVER["REQUEST_METHOD"] != "POST") {
+                        $message = "An error has occured. Please try again.";
+                } elseif (empty($_POST["username"])) {
+                        $message = "Name is required";
+                } else {
+                        $username = $_POST["username"];
+                }
+
+                if (empty($_POST["password"])) {
+                        $message = "Password is required";
+                } else {
+                        $password = $_POST["password"];
+                }
+
+                $sql = "SELECT * FROM admin where username = '$username'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        if ($row["password"] === $password) {
+                                $message = "You have successfully logged in.";
+                        } else {
+                                $message = "Password does not match. Please try again.";
+                        }
+                } else {
+                         $message = "Username $username does not exist. Please try again.";
+                }
+
+                $conn->close();
+                ?>
+
+                <div class="col-sm-8 blog-main">
+                        <p><?php echo $message; ?></p>
+                </div><!-- /.blog-main -->
+
+        </body>
+</html>
